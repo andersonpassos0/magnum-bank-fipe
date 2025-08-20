5bb81480f4c09fc14521f63623a8d23e73af4014
andersonpassos0 <anderson.passos@yahoo.com.br>
2025-08-20 16:06:46 -0300

    ajusta docker-compose, Dockerfile e application.yml

--- api1/src/main/java/br/com/magnumbank/api1/Api1Application.java
+++ api1/src/main/java/br/com/magnumbank/api1/Api1Application.java
@@ -8,6 +8,7 @@ public class Api1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Api1Application.class, args);
+		System.out.println("API 1 rodando...");
 	}
 
 }
--- api2/src/main/java/br/com/magnumbank/api2/Api2Application.java
+++ api2/src/main/java/br/com/magnumbank/api2/Api2Application.java
@@ -8,6 +8,7 @@ public class Api2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Api2Application.class, args);
+		System.out.println("API 2 rodando...");
 	}
 
 }
