d5be26d53f5c6bc31ce0b9323fe465526e190bb7
andersonpassos0 <anderson.passos@yahoo.com.br>
2025-08-21 10:25:54 -0300

    (API1) chore: ajustes no application.yml (show-sql), versionamento da rota e log.info)

--- api1/src/main/java/br/com/magnumbank/api1/adapters/in/MarcaController.java
+++ api1/src/main/java/br/com/magnumbank/api1/adapters/in/MarcaController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/marcas")
+@RequestMapping("/api/v1/marcas")
 @RequiredArgsConstructor
 public class MarcaController {
 
