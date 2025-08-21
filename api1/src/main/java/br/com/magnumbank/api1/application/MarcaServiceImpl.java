0fe7824c6e447a7ab8360b4d357d24a9ea2dbab2
Anderson Passos <anderson.passos@yahoo.com.br>
2025-08-21 17:31:54 -0300

    (API1) feature: implementa cache Redis

--- api1/src/main/java/br/com/magnumbank/api1/application/MarcaServiceImpl.java
+++ api1/src/main/java/br/com/magnumbank/api1/application/MarcaServiceImpl.java
@@ -2,6 +2,7 @@ package br.com.magnumbank.api1.application;
 
 import br.com.magnumbank.api1.adapters.in.dto.MarcaResponse;
 import br.com.magnumbank.api1.adapters.in.dto.VeiculoResponse;
+import br.com.magnumbank.api1.cache.SortUtils;
 import br.com.magnumbank.api1.domain.Marca;
 import br.com.magnumbank.api1.domain.Veiculo;
 import br.com.magnumbank.api1.mapper.MarcaMapper;
@@ -10,9 +11,14 @@ import br.com.magnumbank.api1.ports.out.MarcaRepository;
 import br.com.magnumbank.api1.ports.out.VeiculoRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -25,17 +31,43 @@ public class MarcaServiceImpl implements MarcaService{
 
     @Override
     public Page<MarcaResponse> getMarcas(Pageable pageable) {
-        Page<Marca> marcasList = marcaRepository.findAll(pageable);
-        return marcasList.map(marcaMapper::toResponse);
+        String sortKey = SortUtils.sortToKey(pageable.getSort());
+        List<MarcaResponse> content = getMarcasContentCached(pageable.getPageNumber(), pageable.getPageSize(), sortKey);
+        long total = getMarcasTotalCached();
+        return new PageImpl<>(content, pageable, total);
+    }
+
+    @Cacheable(cacheNames = "marcas_todas", key = "´p:´ + #page + ':s:' + #size + ':O:' + #sortkey")
+    public List<MarcaResponse> getMarcasContentCached(int page, int size, String sortKey){
+        Pageable pageable = PageRequest.of(page, size, SortUtils.keyToSort(sortKey));
+        Page<Marca> pageData = marcaRepository.findAll(pageable);
+        return pageData.map(marcaMapper::toResponse).getContent();
+    }
+
+    @Cacheable(cacheNames = "marcas_total", key = "'v1'")
+    public long getMarcasTotalCached(){
+        return marcaRepository.count();
     }
 
     @Override
     public Page<VeiculoResponse> listarVeiculosPorMarca(String codigo, Pageable pageable) {
-        Marca marca = marcaRepository.findByCodigo(codigo)
-                .orElseThrow(() -> new EntityNotFoundException("Marca de código " + codigo + " não encontrada!"));
+        String sortKey = SortUtils.sortToKey(pageable.getSort());
+        List<VeiculoResponse> content = getVeiculosPorMarcaContentCached(codigo, pageable.getPageNumber(), pageable.getPageSize(), sortKey);
+        long total = getVeiculosPorMarcaTotalCached(codigo);
+        return new PageImpl<>(content, pageable, total);
+    }
 
-        Page<Veiculo> veiculosList = veiculoRepository.findByMarca(marca, pageable);
+    @Cacheable(cacheNames = "veiculos_por_marca", key = "#codigo +'::p:' + #page + ':s:' + #size + ':o:' + #sortKey")
+    private List<VeiculoResponse> getVeiculosPorMarcaContentCached(String codigo, int page, int size, String sortKey) {
+        Pageable pageable = PageRequest.of(page, size,SortUtils.keyToSort(sortKey));
+        var marca = marcaRepository.findByCodigo(codigo)
+                .orElseThrow(() -> new EntityNotFoundException("Marca de código " + codigo + " não encotrada!"));
+        var pageData = veiculoRepository.findByMarca(marca, pageable);
+        return pageData.map(veiculoMapper::toResponse).getContent();
+    }
 
-        return veiculosList.map(veiculoMapper::toResponse);
+    @Cacheable(cacheNames = "veiculos_por_marca_total", key = "#codigo")
+    public long getVeiculosPorMarcaTotalCached(String codigo) {
+        return veiculoRepository.countByMarcaCodigo(codigo);
     }
 }
---
+++ api1/src/main/java/br/com/magnumbank/api1/config/CacheConfig.java
@@ -0,0 +1,31 @@
+package br.com.magnumbank.api1.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+
+@Configuration
+public class CacheConfig {
+
+    @Bean
+    public RedisCacheManager cacheManager(LettuceConnectionFactory cf){
+        ObjectMapper om = new ObjectMapper();
+        om.registerModule(new JavaTimeModule());
+
+        var serializer = new GenericJackson2JsonRedisSerializer(om);
+        var config = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(serializer))
+                .entryTtl(Duration.ofMinutes(10))
+                .disableCachingNullValues();
+
+        return RedisCacheManager.builder(cf).cacheDefaults(config).build();
+    }
+}
